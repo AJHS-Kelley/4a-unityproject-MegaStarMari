ca8891a2b8c67d9d3a5e5d98723c927bb9d23a3d
A.O.Burx <amarionoburks@gmail.com>
2024-02-06 16:06:29 -1100

    Basic Variables

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        int myInt = 0;
+        float myFloat = 3.14F;
+        bool myBool = true;
+        string myString = "I like chicken nuggets";
+
+        Console.WriteLine("This is an integer: " + myInt);
+        Console.WriteLine("This is a float: " + myFloat);
+        Console.WriteLine("This is a boolean: " + myBool);
+        Console.WriteLine("This is a string: " + myString);
+
     }
 }
